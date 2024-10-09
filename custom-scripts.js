883e05bdc3dccbeedd16e8ad5cdd3a5e38147729
masgpt <131375989+masgpt@users.noreply.github.com>
2024-10-09 01:43:07 -0700

    moved-code-from-sg-to-github

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,6 +1,28 @@
-// Custom scripts for ScaleGrowth
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Minimal required head content -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filmclusive</title>
 
-// Display a welcome message
-window.onload = function() {
-    alert('Welcome to our ScaleGrowth platform!');
-};
+</head>
+<body>
+    <!-- Your website content goes here -->
+
+    <!-- Optimized Footer with all meta tags and scripts -->
+
+    <!-- Open Graph meta tags for SEO and social sharing -->
+    <meta property="og:title" content="Filmclusive" />
+    <meta property="og:image" content="https://strongasianlead.com/wp-content/uploads/2024/03/Filmclusive-Logo_App.png" />
+
+    <!-- Favicon Links -->
+    <link rel="icon" href="https://info.filmclusive.com/wp-content/uploads/2024/04/cropped-Filmclusive-Logo_2.0-01_1x1.png" type="image/png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://info.filmclusive.com/wp-content/uploads/2024/04/cropped-Filmclusive-Logo_2.0-01_1x1.png">
+    <link rel="mask-icon" href="https://info.filmclusive.com/wp-content/uploads/2024/04/cropped-Filmclusive-Logo_2.0-01_1x1.png" color="#5bbad5">
+
+    <!-- Start of HubSpot Embed Code -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/46328658.js"></script>
+    <!-- End of HubSpot Embed Code -->
+</body>
+</html>
